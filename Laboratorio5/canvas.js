accf15184e77108f3bf8b9f34be6022f7b612c08
Juan Zambrano <jpablozambrano@hotmail.com>
2024-12-15 19:35:00 +0100

    lab 6

---
+++ Laboratorio5/canvas.js
@@ -0,0 +1,8 @@
+import loadImage from './loadImages.js';
+import SpriteSheet from './SpriteSheet.js';
+
+function main(){
+  let canvas = document.getElementById('lienzo');
+  let context = canvas.getContext('2d');
+  
+};
